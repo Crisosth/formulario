538f24ff7f289bff456cf94c6a30e2ef1c935279
Crisosth <favocrisosth5@gmail.com>
2024-05-29 03:19:36 -0300

    atualizacao

--- app.py
+++ app.py
@@ -18,10 +18,10 @@ def prever():
     idade = int(request.form['age'])
     sexo = int(request.form['sex'])
     tipo_dor_peito = int(request.form['chest_pain'])
-    pressao_sanguinea_repouso = float(request.form['resting_bp'])
-    colesterol_soro = float(request.form['cholesterol'])
+    pressao_sanguinea_repouso = int(request.form['resting_bp'])
+    colesterol_soro = int(request.form['cholesterol'])
     acucar_sangue_jejum = int(request.form['fasting_bs'])
-    frequencia_cardiaca_maxima = float(request.form['max_hr'])
+    frequencia_cardiaca_maxima = int(request.form['max_hr'])
     angina_induzida_exercicio = int(request.form['exercise_angina'])
     inclinacao_st_pico = int(request.form['st_slope'])
 
@@ -32,11 +32,17 @@ def prever():
                       angina_induzida_exercicio, inclinacao_st_pico]])
     
     # Verificar se os dados têm o formato correto (11 features)
-    if dados.shape[1] != 11:
+    if dados.shape[1] != 9:
         return "Erro: Os dados fornecidos não têm o formato correto."
 
     # Fazer a previsão usando o modelo
     previsao = modelo.predict(dados)
+
+    # Verificar se a previsão é unidimensional e expandir se necessário
+    if previsao.ndim == 1:
+        previsao = np.expand_dims(previsao, axis=0)
+
+    # Obter o resultado da previsão
     resultado = np.argmax(previsao, axis=1)
 
     # Interpretar o resultado
